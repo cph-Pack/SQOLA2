5e8dd9cc2f814fb21962c964c6e0e03564e3fb48
Dieky <36627007+Dieky@users.noreply.github.com>
2024-09-13 11:31:09 +0200

    added dependancies to test project

--- TaskManagerAPITests/TaskManagerAPITest.cs
+++ TaskManagerAPITests/TaskManagerAPITest.cs
@@ -1,3 +1,5 @@
+using TaskManagerAPI;
+
 namespace TaskManagerAPITests
 {
     public class TaskManagerAPITest
@@ -5,7 +7,17 @@ namespace TaskManagerAPITests
         [Fact]
         public void Test1()
         {
+            int a = 2; int b = 2;
+            Assert.Equal(a, b);
+        }
 
+        [Fact]
+        public void Test2()
+        {
+            // tester om der er hul igennem. Denne test kan slettes. 
+            TaskManager test = new TaskManager();
+            var data = test.GetAllTasks();
+            Assert.True(true);
         }
     }
 }
